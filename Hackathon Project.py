5b09223cdf66e2507ee58d39566e46e6390bb58a
Weaver711 <123115044+Weaver711@users.noreply.github.com>
2023-04-15 13:39:02 -0500

    Add files via upload

--- Hackathon Project.py
+++ Hackathon Project.py
@@ -1,5 +1,6 @@
 ### Hackathon Project
 ### Blackjack game with powerups, multiplayer pvp or single vs dealer:
+### Fix bug dealer draws ace doesnt revert to 1
 import tkinter as tk
 from tkinter import messagebox
 import random 
@@ -8,6 +9,11 @@ facecardlist = ['j','q','k']
 deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,'a','a','a','a','j','j','j','j','q','q','q','q','k','k','k','k']
 handvalue = 0
 stillplaying = True
+print("This is elemental blackjack here is a rundown of the elements: ")
+print("Team Fire allows you to take away 3 from the opponents final score")
+print("Team Ice allows you to add 3 to your score if you are below 19")
+userelement = input("Would you like to be Fire or Ice?: ")
+userelement.lower()
 class card:
     def __init__(self, denom):
         if denom in numbercardlist:
@@ -23,6 +29,9 @@ while stillplaying == True:
     for ele in dealerhand[0]:
         currentcard = card(ele)
         dealerhandvalue += currentcard.value
+    if dealerhandvalue >21:
+        dealerhandvalue -= 10
+        dealerhand[0][0] = 'Ace'
     userhand = []
     userhand.append(random.choices(deck, k=2))
     handvalue = 0
@@ -33,10 +42,9 @@ while stillplaying == True:
         print("You won! Blackjack")
         stillplaying = False
     elif handvalue >21:
-        print("Your hand value is", handvalue)
-        print("You Lost")
-        stillplaying = False
-    while handvalue < 21 and stillplaying == True:
+        handvalue -= 10
+        userhand[0][0] = 'Ace'
+    while stillplaying == True:
         print("Your cards are: ", userhand)
         print("Your hand value is", handvalue)
         print("The dealers up card is", dealerhand[0][0])
@@ -48,67 +56,114 @@ while stillplaying == True:
             print("You drew a: ", lastcard)
             currentcard = card(lastcard)
             handvalue += currentcard.value
-            if handvalue > 21 and ('a' in userhand):
-                handvalue -= 10
+            if handvalue > 21 and (('a' in userhand) or ('a' in userhand[0])):
+                if 'a' in userhand[0]:
+                    for i in range(len(userhand[0])-1):
+                        if userhand[0][i] == 'a':
+                            userhand[0][i] = 'Ace'
                 for i in range(len(userhand) -1):
                     if userhand[i] =='a':
-                        userhand[i] = 'A'
-                    else:
-                        pass
-            else:
-                pass
+                        userhand[i] = 'Ace'
+                handvalue -= 10
             if handvalue == 21:
                 print("You hit 21!")
-                while dealerhandvalue <16:
+                print("The dealers down card was a", dealerhand[0][1])
+                if dealerhandvalue <=16:
                     dealerhand.append(random.choice(deck))
                     lastcard = dealerhand[(len(dealerhand)-1)]
+                    if dealerhandvalue > 21 and (('a' in dealerhand) or ('a' in dealerhand[0])):
+                        dealerhandvalue -= 10
+                        if 'a' in dealerhand[0]:
+                            for i in range(len(dealerhand[0])-1):
+                                if dealerhand[0][i] == 'a':
+                                    dealerhand[0][i] = 'Ace'
+                                else: pass
+                        for i in range(len(dealerhand) -1):
+                            if dealerhand[i] =='a':
+                                dealerhand[i] = 'Ace'
+                            else:
+                                pass
                     print("The dealer draws a", lastcard)
                     currentcard = card(lastcard)
                     dealerhandvalue += currentcard.value
                 if dealerhandvalue < 21 and dealerhandvalue > 16:
+                    if userelement == 'fire':
+                        dealerhandvalue -= 3
                     if dealerhandvalue < handvalue:
                         print("The dealer ended with", dealerhandvalue, "You Win")
+                        stillplaying = False
                     elif dealerhandvalue > handvalue:
                         print("The dealer ended with", dealerhandvalue, "You Lose")
+                        stillplaying = False
                     else:
                         print("The dealer ended with", dealerhandvalue, "Its a Push")
+                        stillplaying = False
                 if dealerhandvalue > 21:
                     print("You won! the dealer busted")
+                    stillplaying = False
                 elif dealerhandvalue == 21:
+                    if userelement == 'fire':
+                        dealerhandvalue -= 3
+                    print("The dealers down card was a", dealerhand[0][1])
                     print("The dealer hit 21, Its a Push")
+                    stillplaying = False
             elif handvalue > 21:
                 print('You Busted')
-            else:
-                pass
-
-
-
-
+                stillplaying = False
         elif userchoice == 'no':
+            if userelement == 'ice' and handvalue < 19:
+                handvalue += 3
             print("Your ending total was", handvalue )
             print("The dealers down card was a", dealerhand[0][1])
             print("This puts the dealers total at", dealerhandvalue)
-            while dealerhandvalue <16:
-                dealerhand.append(random.choice(deck))
-                lastcard = dealerhand[(len(dealerhand)-1)]
-                print("The dealer draws a", lastcard)
-                currentcard = card(lastcard)
-                dealerhandvalue += currentcard.value
-            if dealerhandvalue < 21 and dealerhandvalue > 16:
-                if dealerhandvalue > handvalue:
-                    print("The dealer ended with", dealerhandvalue, "The dealer wins")
-                elif dealerhandvalue < handvalue:
-                    print("The dealer ended with", dealerhandvalue, "You Win!")
-                else:
-                    print("The dealer ended with", dealerhandvalue, "Its a Push")
-            if dealerhandvalue > 21:
-                print("You won! the dealer busted")
-            stillplaying = False
-
-
-
-
-
+            while stillplaying == True:
+                if dealerhandvalue <= 16:
+                    dealerhand.append(random.choice(deck))
+                    lastcard = dealerhand[(len(dealerhand)-1)]
+                    print("The dealer draws a:", lastcard)
+                    currentcard = card(lastcard)
+                    dealerhandvalue += currentcard.value
+                    if dealerhandvalue > 21 and (('a' in dealerhand) or ('a' in dealerhand[0])):
+                        if 'a' in dealerhand[0]:
+                            for i in range(len(dealerhand[0])):
+                                if dealerhand[0][(i-1)] == 'a':
+                                    dealerhand[0][(i-1)] = 'Ace'
+                        if 'a' in dealerhand:
+                            for i in range(len(dealerhand)):
+                                if dealerhand[(i-1)] =='a':
+                                    dealerhand[(i-1)] = 'Ace'
+                        dealerhandvalue -= 10
+                if dealerhandvalue < 21 and dealerhandvalue > 16:
+                    if userelement == 'fire':
+                        dealerhandvalue -=3
+                        print('You use your fire powers to subtract 3 from the opponents score')
+                    if dealerhandvalue > handvalue:
+                        print("The dealer ended with", dealerhandvalue, "The dealer wins")
+                        stillplaying = False
+                    elif dealerhandvalue < handvalue:
+                        print("The dealer ended with", dealerhandvalue, "You Win!")
+                        stillplaying = False
+                    else:
+                        print("The dealer ended with", dealerhandvalue, "Its a Push")
+                        stillplaying = False
+                if dealerhandvalue > 21:
+                    print("You won! the dealer busted")
+                    stillplaying = False
+                if dealerhandvalue == 21 and userelement == 'fire':
+                    dealerhandvalue -= 3
+                    print('You use your fire powers to subtract 3 from opponents score')
+                    if dealerhandvalue > handvalue:
+                        print("The dealer ended with", dealerhandvalue, "The dealer wins")
+                        stillplaying = False
+                    elif dealerhandvalue < handvalue:
+                        print("The dealer ended with", dealerhandvalue, "You Win!")
+                        stillplaying = False
+                    else:
+                        print("The dealer ended with", dealerhandvalue, "Its a Push")
+                        stillplaying = False
+                if dealerhandvalue == 21:
+                    print("The dealer drew 21, Dealer Wins")
+                    stillplaying = False
     playagain = input("Would you like to play again?: ")
     playagain.lower()
     if playagain == 'yes':
@@ -129,8 +184,3 @@ class mygui:
         frame.pack(fill='x')
         self.root.mainloop()
 mygui()
-    
-
-    
-
-
